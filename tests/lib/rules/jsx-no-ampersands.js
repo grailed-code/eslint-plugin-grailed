d2eac5510890881024582d3b38cd9b64f4350416
Nicu Listana <niculistana@gmail.com>
2021-05-26 15:59:18 -0700

    Adds jsx-no-ampersands
    Adds minimal setup for custom eslint rule.
    
    - Introduces a lint rule `jsx-no-ampersands` copied from on `jsx-embed-condition`: https://github.com/yannickcr/eslint-plugin-react/blob/ee232bbaef4fb04740413d5048877559abf06222/lib/rules/jsx-embed-condition.js
    - TODO: add tests for `jsx-no-ampersands`
    - To use this rule, add the following to `.eslintrc`:
    ```json
     {
      "plugins": [
       "grailed"
      ],
      "rules": {
       "grailed/jsx-no-ampersands": "warn"
      }
     }
    ```
    
    Co-authored-by: Bean <10777333+GeneTheBean@users.noreply.github.com>

---
+++ tests/lib/rules/jsx-no-ampersands.js
@@ -0,0 +1,6 @@
+/**
+ * @todo: add tests
+ * @see: https://github.com/yannickcr/eslint-plugin-react/blob/ee232bbaef4fb04740413d5048877559abf06222/tests/lib/rules/jsx-embed-condition.js
+ * @link: The `eslint-plugin-react` repo uses `instanbul` to run tests: https://istanbul.js.org
+ * NOTE [@niculistana]: Will add tests once we know more about how to run them
+ */
